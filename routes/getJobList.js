237c51de6ba5a3bbd59501b4bd6b0375907ee919
fawazsullia <fawazsullia@gmail.com>
2021-06-28 16:43:33 +0530

    add conditional for /listing without category

--- routes/getJobList.js
+++ routes/getJobList.js
@@ -6,12 +6,25 @@ router.get('/:category', async (req, res)=>{
 
 const category = req.params.category;
 
+if(category!==""){
+
 try{
 const jobs = await submitJobModel.find( {  skills : category } );
 res.status(200).json(jobs).end();
 }
 catch(err){
     res.status(500).json({message : "Could not find data"}).end()
+}
+}
+else {
+    try{
+        const jobs = await submitJobModel.find();
+        res.status(200).json(jobs).end();
+        }
+        catch(err){
+            res.status(500).json({message : "Could not find data"}).end()
+        }
+
 }
 
 
